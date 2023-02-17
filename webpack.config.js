3464910aaba1c68b0e0bbd3cf30ffc76fab62240
Victoria <vitagood.92@list.ru>
2023-02-17 15:36:10 +0300

    Установит и настроит Babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,22 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [{ from: 'public' },]
     }),
   ],
-};
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+
+      }
+    ]
+  }
+}
 
